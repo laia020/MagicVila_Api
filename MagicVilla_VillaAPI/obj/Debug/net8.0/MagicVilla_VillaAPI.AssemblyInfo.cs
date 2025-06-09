396a5dd7a3308c063831188b2655c47350c76fda
Lucas Manentti <lucasmanentti20@gmail.com>
2025-06-09 16:59:49 -0300

    Limpando arquivos .vs e commitando alterações

--- MagicVilla_VillaAPI/obj/Debug/net8.0/MagicVilla_VillaAPI.AssemblyInfo.cs
+++ MagicVilla_VillaAPI/obj/Debug/net8.0/MagicVilla_VillaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+747623c480a8d45be630450c404464bfba18ebb3")]
 [assembly: System.Reflection.AssemblyProductAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MagicVilla_VillaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
