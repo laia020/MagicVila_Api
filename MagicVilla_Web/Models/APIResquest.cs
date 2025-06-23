fabf7e48c00dff81095693e1d6f76b9562cdd7d0
LucasIntexBank <lucas.manentti@intexbank.com.br>
2025-06-23 18:59:29 -0300

    :sparkles: Adicionando arquivos na model e o projeto Utility

---
+++ MagicVilla_Web/Models/APIResquest.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using static MagicVilla_Utility.SD;
+
+namespace MagicVilla_Web.Models
+{
+    public class APIResquest
+    {
+        public ApiType ApiType { get; set; } = ApiType.GET;
+        
+    }
+}
